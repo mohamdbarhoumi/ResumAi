5995e87e3afb87bc837d3a187018edab8a3111db
mohamdbarhoumi <barhoumimohamed@fsg.u-gabes.tn>
2025-10-15 11:40:15 +0100

    Inital commit

---
+++ src/main/java/org/example/resumai/ResumAiApplication.java
@@ -0,0 +1,13 @@
+package org.example.resumai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/resumai/ResumAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.resumai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
